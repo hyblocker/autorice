14478b1c9bf34f2c0dc14b3914589d2772108cb9
Hyblocker <hyblocker@protonmail.com>
2022-05-26 02:39:41 +0200

    Fix dead URLs

--- win/all.cmd
+++ win/all.cmd
@@ -1,14 +1,17 @@
 @ECHO OFF
 SET URL_ROOT=https://rice.hyblocker.dev/win
 
-:: Configure Windows settings as I see fit
-curl %URL_ROOT%/settings/personalization.cmd > cmd
-
 :: Disable automatic updates
 curl %URL_ROOT%/settings/disable-updates.cmd > cmd
 
 :: Configure Windows settings for better performance
 curl %URL_ROOT%/settings/performance.cmd > cmd
+
+:: Configure Windows settings because some of them don't make sense (e.g. fixes download speed bottlenecking)
+curl %URL_ROOT%/settings/config.cmd > cmd
+
+:: Configure Windows settings as I see fit
+curl %URL_ROOT%/settings/personalization.cmd > cmd
 
 :: Download apps I like using
 curl %URL_ROOT%/apps.cmd > cmd
